5c8c27863d74cde28ca78254d0d6becc9fa389a4
Mickey Markoff <81592331+mickeymarkoff@users.noreply.github.com>
2022-02-01 14:53:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Mickey Markoff's Repository</h2>
+          <h2>Welcome to Mickey Markoff's Repo!</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> save to reload.
+        </p>
+      </div>
     );
   }
 }
