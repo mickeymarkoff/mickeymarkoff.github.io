4d3b598b5c0ee46c192f2629989ca3047348a661
Mickey Markoff <81592331+mickeymarkoff@users.noreply.github.com>
2022-02-01 14:49:25 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Mickey Markoff's Repository</h2>
+    );
+  }
+}
+
+export default App;
